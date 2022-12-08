f3e40e9170bdc09cf2cd1d90a3f7b32ad444d46a
Aya <you@example.com>
2022-12-09 11:16:47 +1300

    install tailwind and add custom theme settings to tailwind.config

---
+++ tailwind.config.cjs
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit", // Just-in-Time mode to make the build time much faster
+  darkMode: "media", // or remove this line if darkMode is not used
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['"Inter"', "sans-serif"],
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#51D9D9",
+        },
+        secondary: {
+          DEFAULT: "#9747FF",
+          dark: "#5A328F",
+          2: "#FBBC05",
+        },
+        background: {
+          4: "#424867",
+          3: "#252A41",
+          2: "#1E2235",
+          1: "#1A1D2D",
+        },
+        neutral: {
+          light: "#dcdef3",
+          DEFAULT: "#ABADC6",
+          dark: "#7d7e89",
+        },
+        alert: {
+          DEFAULT: "#EA4335",
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/line-clamp"), require("tailwind-scrollbar")],
+};
