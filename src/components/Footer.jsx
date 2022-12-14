35beb670829e57295235de2afd6f5df351bf0dfe
ethan <ethanjamescullen@outlook.com>
2022-12-14 17:03:26 +1100

    Footer development

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,29 @@
+import React from "react";
+import ItemsContainer from "./footer/ItemsContainer";
+import SocialIcons from "./footer/SocialIcons";
+import { Icons } from "./footer/Menus";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className=" text-white">
+      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4  py-7">
+        <h1
+          className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold
+         md:w-2/5"
+        >
+          </h1>
+          </div>
+      <ItemsContainer />
+      <div
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10
+      text-center pt-2 text-gray-400 text-sm pb-8"
+      >
+        <span>© 2022 LEET. All rights reserved.</span>
+        <span>Terms · Privacy Policy</span>
+        <SocialIcons Icons={Icons}  />
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
---
+++ src/components/Footer/Menus.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { BsTwitch, BsFacebook, BsTwitter, BsDiscord } from 'react-icons/bs'
+
+// Ethan Social Icons componenet and Footer links = Menus 
+export const FAQ = [
+  { name: "How To Sell", link: "#" },
+  { name: "How To Buy", link: "#" },
+  { name: "How To LEET", link: "#" },
+];
+export const RESOURCES = [
+  { name: "Privacy Policy", link: "#" },
+  { name: "Terms & Conditions", link: "#" },
+
+];
+export const COMPANY = [
+   { name: "NEWS", link: "#" },
+  { name: "Our Goal", link: "#" },
+  { name: "About LEET", link: "#" },
+];
+export const SUPPORT = [
+  { name: "Contacts", link: "#" },
+  { name: "Order Support", link: "#" },
+  { name: "Support Tickets", link: "#" },
+
+];
+
+export const Icons = [
+  {
+    name: "logo-facebook", link: "www.facebook.com",
+    icon:{BsFacebook} 
+  },
+  {
+    name: "logo-twitter", link: "www.twitter.com",
+    icon:{BsTwitter}
+  },
+  
+  {
+    name: "logo-Twitch", link: "https://www.twitch.tv/",
+    icon:{BsTwitch}
+  },
+  {
+    name: "logo-instagram", link: "https://discord.com/",
+    icon:{BsDiscord}
+  },
+];
