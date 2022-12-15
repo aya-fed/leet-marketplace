a8d278f586ef6da278b110052b6670562c1d90a1
ethan <ethanjamescullen@outlook.com>
2022-12-15 19:35:15 +1100

    linking the sideNav Ethan finished

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,14 +11,15 @@ export default function Sidebar(){
     
     
 //  Buttons still require some logic and the mobile needs the side Nav to go off screen but I couldn't problem solve this yet something to do with the button on the header menu, 
-    <div className="sidebar bg-[#252A41] h-[100vh] fixed top-0 left-0 overflow-auto flex flex-col  pt-24  w-80  ease-in duration-500 scrollbar-thin z-10 "   > 
+    <div className="sidebar bg-[#252A41] h-[100vh] fixed top-0 left-0 overflow-auto flex flex-col  pt-24  w-80  ease-in duration-500 scrollbar-thin z-10 "
+    > 
       
       <Button label="New listing"
         className=" mt-16 mb-4  scale-75 ">
         New Listing
       </Button>
 
-      {items.map((item, index) => <SidebarItem key={index} item={item} />)}
+      {items.map((item, index) => <SidebarItem key={index} item={item}  />)}
       
       <div
         className="bg-[#fff] h-0.5 opacity-50 w-[90%] self-center align-bottom mt-12 mb-4" >
--- src/data/categories.js
+++ src/data/categories.js
@@ -89,6 +89,7 @@ const itemCategories = [
             unit: "MHz",
           },
           {
+            
             name: "Boost Clock Speed",
             type: "inputNumber",
             unit: "MHz",
