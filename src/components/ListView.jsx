6b1a7d047411005177d9a97bff68094ad32f1112
Aya <you@example.com>
2022-12-15 15:06:57 +1300

    add: category page - display items under the category in the parameter

--- src/components/ListView.jsx
+++ src/components/ListView.jsx
@@ -4,29 +4,42 @@ import itemCategories from "../data/categories";
 import { useFetchItems } from "../hooks/useFetchItems";
 import ItemCard from "./ItemCard";
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
 
-export default function ListView() {
+export default function ListView({ items: propItems }) {
   const categories = itemCategories.slice();
   categories.unshift({ value: "", label: "All" });
   // console.log(categories);
-  const { listings: items } = useFetchItems();
-  // console.log(items);
+  const { getItems, listings } = useFetchItems();
+  const [items, setItems] = useState(propItems ?? []);
+  useEffect(() => {
+    if (!propItems) {
+      getItems();
+    }
+  }, []);
+  useEffect(() => {
+    if(!propItems){
+      setItems(listings);
+    } else {
+      setItems(propItems);
+    }
+  }, [listings, propItems]);
 
   return (
     <div>
       <div>
       
-      <div className="grid grid-cols-1 gap-6 xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 m-20">
+        <div className="grid grid-cols-1 gap-6 xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 m-20">
           {items.map((item, index) => {
             console.log(item);
-        return (
-          <div>
+              return (
+                <div>
             <Link to={`/item-detail/${item.itemId}`} > <ItemCard key={index} item={item} /></Link>
-          </div>
-          );
-        })}
-      </div>
+                </div>
+              );
+            })}
         </div>
+      </div>
     </div>
   );
 }
