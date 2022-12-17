f3d1e45d61eb5d2b7ece1027d36d618f7fc61e4e
Michele-Carter <tumeke4u@gmail.com>
2022-12-17 14:27:20 +1300

    Fixed list view - no image/margins

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -7,11 +7,11 @@ import WishlistIcon from './ui/WishlistIcon'
 function ItemCard({item}) {
     return (
 
-      <div className="flex-col h-90 w-full rounded-lg overflow-hidden shadow-lg bg-background-3 text-neutral-light">
+      <div className="flex-col h-90 w-[200px] min-w-[180px] rounded-lg overflow-hidden bg-background-3 text-neutral-light">
   
         <div className="bg-neutral-light">
           {item.imageUrls && item.imageUrls.length > 0 ? (<img className="w-full h-40" src={item.imageUrls[0]} alt={item.title} />) : (
-            <div className="w-full h-full bg-black">No image</div>)}
+            <div className="w-full h-40 bg-background-2 text-neutral-dark p-2">No image</div>)}
         </div>
   
         <div className="py-2 px-2 text-base h-40">
