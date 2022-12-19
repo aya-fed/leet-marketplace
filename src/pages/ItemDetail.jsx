2d66bcc0983fb81a304675000c09fa8c97bc6677
Michele-Carter <tumeke4u@gmail.com>
2022-12-19 21:01:03 +1300

    Adding styling

--- src/pages/ItemDetail.jsx
+++ src/pages/ItemDetail.jsx
@@ -5,7 +5,7 @@ import LoadingSpinner from "../components/ui/LoadingSpinner";
 import WishlistIcon from "../components/ui/WishlistIcon";
 import { Link } from "react-router-dom";
 import Button from "../components/ui/Button"
-// import Webshare from "../components/ui/Webshare"
+import Webshare from "../components/ui/Webshare"
 
 export default function ItemDetail() {
   const params = useParams();
@@ -32,23 +32,27 @@ export default function ItemDetail() {
   return (
     <div className="flex">
       <div className=""> 
-        <img className="w-[411px] h-[411px] mx-10" src={item.imageUrls[0]} />
-        <div className="flex mt-3 ml-10 w-[411px] justify-between">
-          <img className="w-[127px] h-[110px]" src={item.imageUrls[0]} />
-          <img className="w-[127px] h-[110px]" src={item.imageUrls[0]} />
-          <img className="w-[127px] h-[110px]" src={item.imageUrls[0]} />
+        <img className="w-[611px] h-[611px] mx-10" src={item.imageUrls[0]} />
+        <div className="flex mt-3 ml-10 w-[611px] mx-10 justify-between">
+          <img className="w-[110px] h-[110px]" src={item.imageUrls[0]} />
+          <img className="w-[110px] h-[110px]" src={item.imageUrls[0]} />
+          <img className="w-[110px] h-[110px]" src={item.imageUrls[0]} />
+          <img className="w-[110px] h-[110px]" src={item.imageUrls[0]} />
+          <img className="w-[110px] h-[110px]" src={item.imageUrls[0]} />
         </div>        
       </div>
-      <div className="w-[800px] h-[725px] overflow-y-scroll">
-        <div className="text-2xl mb-[30px]">{item.title}</div>
-        <div>
+      <div className="w-[800px] h-[725px]">
+        <div className="text-2xl mb-[20px]">{item.title}</div>
+        <div className="flex items-center justify-between mb-5">
           <div className="text-2xl text-primary">${item.price}</div>
-          {/* <div><Webshare /></div> */}
-          <Link to="/wishlist"><WishlistIcon size={20}/></Link>
-          <Button>Buy now</Button>
+          <div className="flex items-center gap-6">
+            <div><Webshare /></div>
+            <div><WishlistIcon size={20}/></div>
+            <Button className="px-4">Buy now</Button>
+          </div>
         </div>
         
-        <div>{descText}</div>
+        <div className="h-[611px] overflow-auto pr-10">{descText}</div>
       </div>
     </div>
   );
