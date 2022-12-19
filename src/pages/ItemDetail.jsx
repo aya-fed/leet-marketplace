118491b06ea359de97f7bbcdf67e7dc9cd9685ec
Michele-Carter <tumeke4u@gmail.com>
2022-12-19 17:27:17 +1300

    Adding icons and webshare

--- src/pages/ItemDetail.jsx
+++ src/pages/ItemDetail.jsx
@@ -1,6 +1,11 @@
+
 import { useFetchOneItem } from "../hooks/useFetchOneItem";
 import { useParams } from "react-router";
 import LoadingSpinner from "../components/ui/LoadingSpinner";
+import WishlistIcon from "../components/ui/WishlistIcon";
+import { Link } from "react-router-dom";
+import Button from "../components/ui/Button"
+import Webshare from "../components/ui/Webshare"
 
 export default function ItemDetail() {
   const params = useParams();
@@ -27,19 +32,22 @@ export default function ItemDetail() {
   return (
     <div className="flex">
       <div className=""> 
-        <img className="w-[611px] h-[611px] mx-10" src={item.imageUrls[0]} />
-        <div className="flex mt-3 ml-10 w-[611px] justify-between">
-          <img className="w-[110px] h-[110px]" src={item.imageUrls[0]} />
-          <img className="w-[110px] h-[110px]" src={item.imageUrls[0]} />
-          <img className="w-[110px] h-[110px]" src={item.imageUrls[0]} />
-          <img className="w-[110px] h-[110px]" src={item.imageUrls[0]} />
-          <img className="w-[110px] h-[110px]" src={item.imageUrls[0]} />
+        <img className="w-[411px] h-[411px] mx-10" src={item.imageUrls[0]} />
+        <div className="flex mt-3 ml-10 w-[411px] justify-between">
+          <img className="w-[127px] h-[110px]" src={item.imageUrls[0]} />
+          <img className="w-[127px] h-[110px]" src={item.imageUrls[0]} />
+          <img className="w-[127px] h-[110px]" src={item.imageUrls[0]} />
         </div>        
       </div>
-      <div className="w-[611px] h-[725px] overflow-scroll">
-        <div>{item.title}</div>
-        <div>${item.price}</div>
-        <div></div>
+      <div className="w-[800px] h-[725px] overflow-y-scroll">
+        <div className="text-2xl mb-[30px]">{item.title}</div>
+        <div>
+          <div className="text-2xl text-primary">${item.price}</div>
+          <div><Webshare /></div>
+          <Link to="/wishlist"><WishlistIcon size={20}/></Link>
+          <Button>Buy now</Button>
+        </div>
+        
         <div>{descText}</div>
       </div>
     </div>
